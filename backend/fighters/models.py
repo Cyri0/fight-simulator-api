328ea2e8296c030e7ccda576cdfa9c1447d349b3
Cyri0 <nemestamas94@gmail.com>
2023-10-11 11:09:03 +0200

    models created (Race, Weapon, Fighter)

--- backend/fighters/models.py
+++ backend/fighters/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Race(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Weapon(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+
+class Fighter(models.Model):
+    name = models.CharField(max_length=255)
+    race = models.ManyToManyField(Race)
+    weapon = models.ManyToManyField(Weapon)
+    strength = models.IntegerField(default=5)
+    intelligence = models.IntegerField(default=5)
+    agility = models.IntegerField(default=5)
+    dexterity = models.IntegerField(default=5)
+    endurance = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.name
+
